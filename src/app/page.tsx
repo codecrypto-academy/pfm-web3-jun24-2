d2701dc9d43fd15be3f4a1fd3f9764f5303f5c72
ewalterp <epowosino@gmail.com>
2024-06-20 19:38:09 +0200

    FirstCommit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-import ConnectWallet from '@/components/ConnectWallet'
+import React from "react";
+import MainBody from "./components/MainBody";
 
-export default function Home() {
-  return <div className='bg-slate-500 h-screen grid content-center justify-center'>
-    <div className='border border-black'>Home para conectar la wallet</div>
-    <ul className="list-disc list-inside">
-      <li>Si está registrado, redirecciona al dashboard del rol correspondiente</li>
-      <li>Si no está registrado, le invitamos a que realice una donación o que se registre como una empresa</li>
-    </ul>
-    <ConnectWallet></ConnectWallet>
+const HomePage = () => (
+  <div className="flex flex-col min-h-screen">
+    <main className="flex-grow">
+      <MainBody />
+    </main>
   </div>
-}
+);
+
+export default HomePage;
